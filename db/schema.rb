0e0746ba3141e8a00116d608eeef7c069a842e47
issa-clown <kkobayashi@iplab.cs.tsukuba.ac.jp>
2017-08-03 16:10:22 +0900

    Add oharada

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170802055431) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  # enable_extension "plpgsql"
 
   create_table "positions", force: :cascade do |t|
     t.string "serial"
