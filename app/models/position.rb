847e905eb38006487bd3ce9d95ee1fede6acc027
prpr-man <oharada@live.jp>
2017-08-07 14:26:43 +0900

    as_jsonにserial情報付与

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,5 @@
 class Position < ApplicationRecord
   def as_json(options={})
-    { id: self.id, latitude: self.latitude.to_f, longitude: self.longitude.to_f, created_at: self.created_at, updated_at: self.updated_at }
+    { id: self.id, serial: self.serial, latitude: self.latitude.to_f, longitude: self.longitude.to_f, created_at: self.created_at, updated_at: self.updated_at }
   end
 end
